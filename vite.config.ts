5f889e55165802637f09ca3cc7cd4a261a98d687
iblabd <iqbalabdurrahman04@gmail.com>
2025-08-11 11:56:23 +0700

    chore: configure react compiler

--- vite.config.ts
+++ vite.config.ts
@@ -1,17 +1,28 @@
-import { defineConfig } from 'vite'
 import { tanstackStart } from '@tanstack/react-start/plugin/vite'
-import viteReact from '@vitejs/plugin-react-swc'
-import viteTsConfigPaths from 'vite-tsconfig-paths'
 import tailwindcss from '@tailwindcss/postcss'
+import viteReact from '@vitejs/plugin-react-swc'
 import autoprefixer from 'autoprefixer'
+import { defineConfig } from 'vite'
+import babel from 'vite-plugin-babel'
+import viteTsConfigPaths from 'vite-tsconfig-paths'
 
 const config = defineConfig({
   css: {
     postcss: {
       plugins: [tailwindcss, autoprefixer],
-    }
+    },
   },
   plugins: [
+    {
+      ...babel({
+        filter: /\.tsx?$/,
+        babelConfig: {
+          presets: ['@babel/preset-typescript'],
+          plugins: ['babel-plugin-react-compiler'],
+        },
+      }),
+      apply: 'build',
+    },
     viteTsConfigPaths({
       projects: ['./tsconfig.json'],
     }),
